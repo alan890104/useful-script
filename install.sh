054af07885b6293e069522c45cca68962c6078b7
alan890104 <alan890104@gmail.com>
2023-05-29 15:48:08 +0800

    feat: add sudo check and install cuda command

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Please run this script with sudo or as root."
+    exit 1
+fi
+
 source ./app/preinstall.sh
 source ./app/python.sh
 source ./app/docker.sh
